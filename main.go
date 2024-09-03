a3066b4e17dede800e1c71fc95b14b600d9a7036
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2024-09-03 20:27:15 +0200

    rewrite as ticket

--- main.go
+++ main.go
@@ -21,20 +21,24 @@ func main() {
 
 	prev := []byte("0")
 
+	ticker := time.NewTicker(250 * time.Millisecond)
+
 	for {
-		_, err = f.Seek(0, io.SeekStart)
-		if err != nil {
-			logger.Fatalf("Could not seek back to beginning of file: %v", err)
+		select {
+		case <-ticker.C:
+			_, err = f.Seek(0, io.SeekStart)
+			if err != nil {
+				logger.Fatalf("Could not seek back to beginning of file: %v", err)
+			}
+			b := make([]byte, 1)
+			n, err := f.Read(b)
+			if err != nil {
+				logger.Fatalf("Could not read bytes: %v, %d", err, n)
+			}
+			if !bytes.Equal(b, prev) {
+				logger.Printf("Read %v\n", string(b))
+				prev = b
+			}
 		}
-		b := make([]byte, 1)
-		n, err := f.Read(b)
-		if err != nil {
-			logger.Fatalf("Could not read bytes: %v, %d", err, n)
-		}
-		if !bytes.Equal(b, prev) {
-			logger.Printf("Read %v\n", string(b))
-			prev = b
-		}
-		time.Sleep(time.Millisecond * 250)
 	}
 }
