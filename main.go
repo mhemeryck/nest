d46ec9ca33b0bcc2f9f30a651c177c71c816680e
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2024-09-09 17:20:53 +0200

    crawling for metas

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"io/fs"
 	"log"
 	"path/filepath"
 	"regexp"
+	"strconv"
 	"time"
 
 	"github.com/mhemeryck/nest/pkg/device"
@@ -16,6 +18,10 @@ const (
 
 var (
 	filenameRegex = regexp.MustCompile("/io_group(1|2|3)/(?P<device_fmt>di|do|ro)_(?P<io_group>1|2|3)_(?P<number>[0-9]{2})/(di|do|ro)_value$")
+
+	DeviceFormat_DigitalInput  = DeviceFormat("DigitalInput")
+	DeviceFormat_DigitalOutput = DeviceFormat("DigitalOutput")
+	DeviceFormat_RelayOutput   = DeviceFormat("RelayOutput")
 )
 
 func main2() {
@@ -45,17 +51,79 @@ func main2() {
 	}
 }
 
+type DeviceFormat string
+type IOGroup int
+type DeviceNumber int
+
+type DeviceMeta struct {
+	Format DeviceFormat
+	Group  IOGroup
+	Number DeviceNumber
+}
+
+func DeviceMetaFromPath(path string) (DeviceMeta, error) {
+	match := filenameRegex.FindStringSubmatch(path)
+	if len(match) == 0 {
+		return DeviceMeta{}, fmt.Errorf("No device matched path")
+	}
+
+	d := DeviceMeta{}
+
+	for k, name := range filenameRegex.SubexpNames() {
+		if k != 0 && name != "" {
+			switch name {
+			case "device_fmt":
+				switch match[k] {
+				case "di":
+					d.Format = DeviceFormat_DigitalInput
+
+				case "do":
+					d.Format = DeviceFormat_DigitalOutput
+
+				case "ro":
+					d.Format = DeviceFormat_RelayOutput
+				}
+			case "io_group":
+				i, err := strconv.Atoi(match[k])
+				if err != nil {
+					return d, err
+				}
+				d.Group = IOGroup(i)
+			case "number":
+				i, err := strconv.Atoi(match[k])
+				if err != nil {
+					return d, err
+				}
+				d.Number = DeviceNumber(i)
+			}
+		}
+	}
+
+	return d, nil
+}
+
 func main() {
+	// paths := make([]string, 0)
+	metas := make([]DeviceMeta, 0)
 	err := filepath.WalkDir("test/fixtures",
 		func(path string, d fs.DirEntry, err error) error {
 			if err != nil {
 				log.Fatalf("Error walking %v", err)
 			}
-			log.Printf("%v - %v\n", path, d)
+			// if filenameRegex.MatchString(path) {
+			// 	log.Printf("%v - %v\n", path, d.Name())
+			// 	paths = append(paths, path)
+			// }
+			meta, err := DeviceMetaFromPath(path)
+			if err == nil {
+				log.Printf("%v - %v - %v\n", path, d.Name(), meta)
+				metas = append(metas, meta)
+			}
+
 			return nil
 		})
 	if err != nil {
 		panic(err)
 	}
-
+	log.Printf("Found %d matching paths", len(metas))
 }
