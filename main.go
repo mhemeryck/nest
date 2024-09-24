4c1c79ee0fd29dbb748cd7247eb5c28fa3d611ed
Martijn Hemeryck <martijn.hemeryck@gmail.com>
2024-09-24 21:49:36 +0200

    some experiments with a hive mind

--- main.go
+++ main.go
@@ -12,9 +12,96 @@ var (
 	automations = map[string]string{
 		"do-1-01": "ro-2-13",
 	}
+
+	pushButtonToLight = map[EntityId]EntityId{
+		"office": "office-desk",
+	}
 )
 
-func main() {
+type EntityId string
+
+type Event struct {
+	// TODO: dedicated type?
+	EntityId EntityId
+	// TODO: enum-like message?
+	Message string
+}
+
+// Hive is the hive mind that knows all
+type Hive struct {
+	EventC chan Event
+
+	pushButtonToLightMap map[EntityId]EntityId
+	PushButtons          map[EntityId]*PushButton
+	Lights               map[EntityId]*Light
+}
+
+func (h Hive) Loop() {
+	for msg := range h.EventC {
+		log.Printf("received msg %v", msg)
+		if id, ok := h.pushButtonToLightMap[msg.EntityId]; ok {
+			log.Printf("Did get a match: %s -> %s!", msg.EntityId, id)
+			if l, ok := h.Lights[id]; ok {
+				l.Toggle()
+				log.Printf("Here's the light %s", l)
+			}
+		}
+	}
+}
+
+type Entity struct {
+	Id     EntityId
+	EventC chan Event
+}
+
+type PushButton struct {
+	Id     EntityId
+	EventC chan Event
+	// TODO: enum-like; with 3-state?
+	State bool
+}
+
+func (p *PushButton) Toggle() {
+	// Change current state
+	p.State = !p.State
+
+	var m string
+	switch p.State {
+	case false:
+		m = "off"
+	case true:
+		m = "on"
+	}
+	p.EventC <- Event{
+		EntityId: p.Id,
+		Message:  m,
+	}
+}
+
+type Light struct {
+	Entity
+
+	// TODO: enum
+	State bool
+}
+
+func (l *Light) Toggle() {
+	l.State = !l.State
+
+	// var m string
+	// switch l.State {
+	// case false:
+	// 	m = "off"
+	// case true:
+	// 	m = "on"
+	// }
+	// l.EventC <- Event{
+	// 	EntityId: l.Id,
+	// 	Message:  m,
+	// }
+}
+
+func main2() {
 	reader := make(chan device.DevicePayload)
 	mgr, err := device.NewDeviceManagerFromPath("test/fixtures", reader)
 	if err != nil {
@@ -52,5 +139,41 @@ func main() {
 	for i := 0; i < 5; i++ {
 		d.Write(i%2 == 0)
 		time.Sleep(3 * time.Second)
+	}
+}
+
+func main() {
+	eventChan := make(chan Event)
+	p := PushButton{
+		Id:     "office",
+		EventC: eventChan,
+		State:  false,
+	}
+
+	l := Light{
+		Entity: Entity{
+			Id:     "office-desk",
+			EventC: eventChan,
+		},
+		State: false,
+	}
+
+	hive := Hive{
+		EventC:               eventChan,
+		pushButtonToLightMap: pushButtonToLight,
+		PushButtons: map[EntityId]*PushButton{
+			p.Id: &p,
+		},
+		Lights: map[EntityId]*Light{
+			l.Id: &l,
+		},
+	}
+
+	go hive.Loop()
+
+	for {
+		p.Toggle()
+
+		time.Sleep(time.Millisecond * 500)
 	}
 }
